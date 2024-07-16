b638c638f24b5a5d6042c6e84d124ea7f80e198a
nasiri-py <sinanasiri.py@gmail.com>
2024-07-16 10:05:09 +0330

    add contact list method

--- contacts.py
+++ contacts.py
@@ -25,4 +25,12 @@ class ContactManager:
             console.print(f"[red]{name} has been removed from the contact list.[/red]", end="\n\n", style="bold")
         else:
             console.print(f"[yellow]{name} is not in the contact list.[/yellow]", end="\n\n", style="bold")
+
+    def list_contacts(self):
+        i = 1
+        for name, contact in self.contacts.items():
+            console.print(f"{i}. [green]{name} : [/green](phone: {contact['phone']}) - (email: {contact['email']}) - (address:  {contact['address']})", style="bold")
+            i += 1
+        else:
+            console.print(f"[yellow]Contact list is empty.[/yellow]", style="bold")
     
