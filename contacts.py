aad099c4beddd0920e7fca8ab1cdef02552e5c46
nasiri-py <sinanasiri.py@gmail.com>
2024-07-16 10:04:12 +0330

    create ContactManager class and deplement add, remove methods

---
+++ contacts.py
@@ -0,0 +1,28 @@
+from rich.console import Console
+
+console = Console()
+
+
+class ContactManager:
+
+    def __init__(self):
+        self.contacts = {}
+
+    def add_contact(self, name, phone, email=None, address=None):
+        if name in self.contacts:
+            console.print(f"[yellow]{name} is already in the contact list.[/yellow]", end="\n\n", style="bold")
+            return
+        self.contacts[name] = {
+            'phone': phone,
+            'email': email,
+            'address': address
+        }
+        console.print(f"[green]{name} has been added to the contact list.[/green]", end="\n\n", style="bold")
+
+    def remove_contact(self, name):
+        if name in self.contacts:
+            del self.contacts[name]
+            console.print(f"[red]{name} has been removed from the contact list.[/red]", end="\n\n", style="bold")
+        else:
+            console.print(f"[yellow]{name} is not in the contact list.[/yellow]", end="\n\n", style="bold")
+    
